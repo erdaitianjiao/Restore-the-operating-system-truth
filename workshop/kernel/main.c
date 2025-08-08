122b57d9c99a46a41f903784f7f20f85285e3dde
sandaitianjiao <540789889@qq.com>
2025-08-08 04:59:28 -0700

    2025.8.8-2

---
+++ workshop/kernel/main.c
@@ -0,0 +1,29 @@
+#include "print.h"
+
+int main() {
+
+    put_char('k');
+    put_char('e');
+    put_char('r');
+    put_char('n');
+    put_char('e');
+    put_char('l');
+    put_char('!');
+    put_char('\n');
+    put_char('i');
+    put_char('m');
+    put_char(' ');
+    put_char('c');
+    put_char('o');
+    put_char('m');
+    put_char('1'); //这里的1是看退格是否生效 
+                    //输出时只要是kernel! im coming!即可
+    put_char('\b');
+    put_char('i');
+    put_char('n');
+    put_char('g');
+    put_char('!');
+    while(1);
+    return 0;
+    
+}
